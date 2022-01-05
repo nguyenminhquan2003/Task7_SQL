2cdcb7cb77f9df7210a0fb0a22d42916064dcfb2
nguyenminhquan2003 <91305705+nguyenminhquan2003@users.noreply.github.com>
2022-01-05 11:38:18 +0700

    Update Lab10.sql

--- Lab10.sql
+++ Lab10.sql
@@ -55,4 +55,14 @@ CREATE INDEX ClassLabIndex ON Classes(Class,Lab)
 GO
 
 --5.Viết câu lệnh xem toàn bộ các chỉ mục của cơ sở dữ liệu Aptech.
-
+SELECT DB_NAME() AS Database_Name
+, sc.name AS Schema_Name
+, o.name AS Table_Name
+, i.name AS Index_Name
+, i.type_desc AS Index_Type
+FROM sys.indexes i
+INNER JOIN sys.objects o ON i.object_id = o.object_id
+INNER JOIN sys.schemas sc ON o.schema_id = sc.schema_id
+WHERE i.name IS NOT NULL
+AND o.type = 'U'
+ORDER BY o.name, i.type
